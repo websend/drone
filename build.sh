b6f70ca5fcc5e4265f6c3679a47691a7ef7179f3
Calvin Wong-Loi-Sing <c.wongloising@websend.nl>
2020-07-16 21:32:05 +0200

    Reusable build file

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+version=$1
+
 export CGO_ENABLED=0
-go build -tags "nolimit" -o release/linux/drone-server github.com/drone/drone/cmd/drone-server
-docker build --rm -f docker/Dockerfile.server.linux.amd64 -t websend/drone:1.7.0.1 .
-docker push websend/drone:1.7.0.1
+
+go build -tags "nolimit" -o release/linux/amd64/drone-server github.com/drone/drone/cmd/drone-server
+docker build --rm -f docker/Dockerfile.server.linux.amd64 -t websend/drone:$version .
+docker push websend/drone:$version
 
